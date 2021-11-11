4aa3db02574c1c3c150bcd17203577e4215bdd52
Kaiser <yannick-kaiser@gmx.de>
2021-11-11 18:08:46 +0100

    Formatting fixes

--- src/test/java/de/exxcellent/challenge/CSVDataLoaderTest.java
+++ src/test/java/de/exxcellent/challenge/CSVDataLoaderTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * JUnit 5 Tests for {@link CSVDataLoader}
+ * 
  * @author Yannick Kaiser <yannick-kaiser@gmx.de>
  */
 class CSVDataLoaderTest {
@@ -22,49 +23,52 @@ class CSVDataLoaderTest {
 	private static Path emptyCSV = Path.of("src/test/resources/de/exxellent/challenge/empty.csv");
 	private static Path validCSV = Path.of("src/test/resources/de/exxellent/challenge/valid.csv");
 	private static Path headerlessCSV = Path.of("src/test/resources/de/exxellent/challenge/headerless.csv");
-	
+
 	@BeforeAll
 	static void setup() {
-		System.out.println(" -- CSVFileLoader Tests --");	
+		System.out.println(" -- CSVFileLoader Tests --");
 	}
-	
-    @Test
-    void emptyFileTest() {	
-    	
-    	System.out.println("  - Empty CSV Tests -");
-    	
-    	// header expected, but empty on streamOfRows
-    	CSVDataLoader testSubject1 = new CSVDataLoader(emptyCSV, null, true);
-    	Exception e1 = assertThrows(IOException.class, () -> testSubject1.streamOfRows().collect(Collectors.toList()));
-    	assertEquals("CSVDataLoader tried to read the header line, but the file is empty", e1.getMessage());
-    	
-    	// empty on getHeaderLine
-    	CSVDataLoader testSubject2 = new CSVDataLoader(emptyCSV, null, true);
-    	Exception e2 = assertThrows(IOException.class, () -> testSubject2.getHeaderLine());
-    	assertEquals("CSVDataLoader tried to read the header line, but the file is empty", e2.getMessage());
-    
-    }
-    
-    @Test
-    void badPathTest() {
-    	
-    	System.out.println("  - Bad Path Test -");
-    	
-    	CSVDataLoader testSubject = new CSVDataLoader(Path.of(""), null, true);
-    	Exception e = assertThrows(IOException.class, () -> testSubject.streamOfRows().collect(Collectors.toList()));
-    }
-    
-    @Test
-    void validDataTest() {
-    	
-    	System.out.println("  - Valid File Test -");
-    	
-    	// valid file, 2 rows x 3 columns with header
-    	CSVDataLoader testSubject = new CSVDataLoader(validCSV, null, true);
-    	
-    	assertDoesNotThrow(() -> assertEquals(testSubject.streamOfRows().count(), 2));
-    	
-    	try {
+
+	@Test
+	void emptyFileTest() {
+
+		System.out.println("  - Empty CSV Tests -");
+
+		// header expected, but empty on streamOfRows
+		CSVDataLoader testSubject1 = new CSVDataLoader(emptyCSV, null, true);
+		Exception e1 = assertThrows(IOException.class, () -> testSubject1.streamOfRows().collect(Collectors.toList()));
+		assertEquals("CSVDataLoader tried to read the header line, but the file is empty", e1.getMessage());
+
+		// empty on getHeaderLine
+		CSVDataLoader testSubject2 = new CSVDataLoader(emptyCSV, null, true);
+		Exception e2 = assertThrows(IOException.class, () -> testSubject2.getHeaderLine());
+		assertEquals("CSVDataLoader tried to read the header line, but the file is empty", e2.getMessage());
+
+	}
+
+	@Test
+	void badPathTest() {
+
+		System.out.println("  - Bad Path Test -");
+
+		CSVDataLoader testSubject = new CSVDataLoader(Path.of(""), null, true);
+		Exception e = assertThrows(IOException.class, () -> testSubject.streamOfRows().collect(Collectors.toList()));
+	}
+
+	/**
+	 * Tests the CSVDataLoader on valid files with headers.
+	 */
+	@Test
+	void validDataTest() {
+
+		System.out.println("  - Valid File Test -");
+
+		// valid file, 2 rows x 3 columns with header
+		CSVDataLoader testSubject = new CSVDataLoader(validCSV, null, true);
+
+		assertDoesNotThrow(() -> assertEquals(testSubject.streamOfRows().count(), 2));
+
+		try {
 			List<String[]> rows = testSubject.streamOfRows().collect(Collectors.toList());
 			assertEquals(rows.get(0)[0], "Arsenal");
 			assertEquals(rows.get(0).length, 3);
@@ -72,34 +76,36 @@ class CSVDataLoaderTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    	
-    	assertDoesNotThrow(() -> testSubject.getHeaderLine());
-    	
-    	try {
+
+		assertDoesNotThrow(() -> testSubject.getHeaderLine());
+
+		try {
 			var header = testSubject.getHeaderLine();
 			assertEquals(header.get()[0], "Team");
 			assertEquals(header.get()[2], "Wins");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-    
-    
-    @Test
-    void headerlessDataTest() {
-    	
-    	System.out.println("  - Headerless File Test -");
-    	
-    	// valid file, 2 rows x 2 columns, no header
-    	CSVDataLoader testSubject = new CSVDataLoader(headerlessCSV, null);
-    	
-    	assertDoesNotThrow(() -> testSubject.getHeaderLine());
-    	
-    	try {
+	}
+
+	/**
+	 * Tests the CSVDataLoader on valid, but headerless files.
+	 */
+	@Test
+	void headerlessDataTest() {
+
+		System.out.println("  - Headerless File Test -");
+
+		// valid file, 2 rows x 2 columns, no header
+		CSVDataLoader testSubject = new CSVDataLoader(headerlessCSV, null);
+
+		assertDoesNotThrow(() -> testSubject.getHeaderLine());
+
+		try {
 			assertEquals(Optional.empty(), testSubject.getHeaderLine());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	
-    }
+
+	}
 }
