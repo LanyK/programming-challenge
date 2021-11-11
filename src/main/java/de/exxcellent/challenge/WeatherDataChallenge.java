33b221d61d70c9785be88e30dec11154645b869d
Kaiser <yannick-kaiser@gmx.de>
2021-11-11 21:10:22 +0100

    Fix WeatherDataChallenge stump

---
+++ src/main/java/de/exxcellent/challenge/WeatherDataChallenge.java
@@ -0,0 +1,11 @@
+package de.exxcellent.challenge;
+
+public class WeatherDataChallenge implements ChallengeTask {
+
+	@Override
+	public void executeTask(FixedSizeTable dataTable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
