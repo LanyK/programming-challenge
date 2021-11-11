b85a52c09a517deba2b1aecd7846b129564c0cde
Kaiser <yannick-kaiser@gmx.de>
2021-11-11 19:24:23 +0100

    Refactor completion & setHeader function for FixedSizeTable

--- src/main/java/de/exxcellent/challenge/FixedSizeTable.java
+++ src/main/java/de/exxcellent/challenge/FixedSizeTable.java
@@ -6,38 +6,40 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * Data class representing a 2D table of data, optionally with column names. The
- * column count of the table is fixed either by providing column header names at
+ * Data class representing a fixed size 2D table of data, optionally with column names.<br> 
+ * The column count of the table is fixed either by providing column header names at
  * creation time,<br>
  * or in the absence of column headers taken from the first row which is added
- * to the table.<br>
- * <br>
+ * to the table.<br><br>
  * 
- * This table implementation uses an underlying row-wise list of String
- * arrays,<br>
+ * This table implementation uses an underlying row-wise list of String arrays,<br>
  * making row lookup and field lookup O(1) and column lookup O(n)
  * 
  * @author Yannick Kaiser <yannick-kaiser@gmx.de>
  */
-public class Table {
+public class FixedSizeTable {
 
 	private Optional<String[]> columnHeaders = Optional.empty();
 	private ArrayList<String[]> rows = new ArrayList<>();
 
-	/**
-	 * Creates a new empty table ready to be populated. TODO
+	/**Create a new empty table ready to be populated.
 	 * 
+	 * While the table is empty, the first added row can be of arbitrary dimensions<br>
+	 * and will fix the width of the Table from then on.<br><br>
+	 * 
+	 * If however column headers are set before any rows are added, the number of headers will<br>
+	 * fix the amount of columns in this fixed size table.
 	 */
-	public Table() {
-
-	}
+	public FixedSizeTable() {}
 
 	/**
-	 * TODO
+	 * Creates a new empty table with preset column headers.<br>
+	 * This also fixes the column count in place; any rows entered must have the same amount of columns.<br>
+	 * Access the width of this table at any point with {@link #getColumnCount()}.
 	 * 
-	 * @param columnHeaders
+	 * @param columnHeaders <code>String[]</code> containing the column names for this Table
 	 */
-	public Table(String[] columnHeaders) {
+	public FixedSizeTable(String[] columnHeaders) {
 		this.setColumnHeaders(columnHeaders);
 	}
 
@@ -67,10 +69,44 @@ public class Table {
 		return this.rows.get(0).length;
 	}
 
+	/**Changes a single column header to the specified value.<br>
+	 * To change a header, the Table must already have headers set via {@link #setColumnHeaders(String[])},<br>
+	 * otherwise a <code>NoSuchElementException</code> will be thrown.
+	 * 
+	 * @param columnIndex
+	 * @param columnHeader
+	 * 
+	 * @throws {@link NoSuchElementException}, {@link IndexOutOfBoundsException}
+	 */
+	public void setHeader(int columnIndex, String columnHeader) {
+		if (!this.hasColumnHeaders()) {
+			throw new NoSuchElementException("Can't set a specific column header when no column headers are set. Set in bulk."); // TODO this is very unfriendly design and should gracefully set a single header
+		}
+		
+		this.columnHeaders.get()[columnIndex] = columnHeader;
+	}
+	
+	/**Arbitrary column headers can only be set when the Table holds no rows.<br>
+	 * In any other case, the fixed column count can't be altered.<br>
+	 * Use {@link #setHeader(int, String)} to alter a single column's header.
+	 * 
+	 * @param columnHeaders
+	 * @throws IndexOutOfBoundsException if the column count of the new array mismatches the size of the table
+	 */
 	public void setColumnHeaders(String[] columnHeaders) {
-		this.columnHeaders = Optional.of(columnHeaders);
+		
+		// always allowed when the table is empty, otherwise needs to adhere to the dimensionality of this Table
+		if (this.getRowCount() == 0 || columnHeaders.length == this.getColumnCount()) {
+			this.columnHeaders = Optional.of(columnHeaders);
+		}
+		
+		// otherwise illegal	
+		throw new IndexOutOfBoundsException("New column header count mismatches existing fixed column count");
 	}
 
+	/**
+	 * @return true if this Table has column headers set
+	 */
 	public boolean hasColumnHeaders() {
 		return !this.columnHeaders.isEmpty();
 	}
@@ -113,7 +149,7 @@ public class Table {
 	 * This operation returns in constant time O(1).
 	 * 
 	 * @param rowIndex
-	 * @return a single row of this {@link Table}
+	 * @return a single row of this {@link FixedSizeTable}
 	 * 
 	 * @throws {@link IndexOutOfBoundsException} if the index is out of range of 0
 	 *                <= index < row count
@@ -131,7 +167,7 @@ public class Table {
 	 * This method builds the column on request in O(n)
 	 * 
 	 * @param columnIndex of the requested column
-	 * @return a single column of this {@link Table}
+	 * @return a single column of this {@link FixedSizeTable}
 	 * 
 	 * @throws {@link IndexOutOfBoundsException}
 	 */
@@ -151,7 +187,7 @@ public class Table {
 	 * 
 	 * @param rowIndex    starting at 0
 	 * @param columnIndex starting at 0
-	 * @return a single row of this {@link Table}
+	 * @return a single row of this {@link FixedSizeTable}
 	 * 
 	 * @throws {@link IndexOutOfBoundsException} if the index is out of range of
 	 *                either the number of rows or columns
