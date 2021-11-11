53d0f4d5b53c2ccda4aeba9c7665605655289f6f
Kaiser <yannick-kaiser@gmx.de>
2021-11-11 21:26:50 +0100

    Add docstring to ChallengeTask interface

--- src/main/java/de/exxcellent/challenge/ChallengeTask.java
+++ src/main/java/de/exxcellent/challenge/ChallengeTask.java
@@ -1,8 +1,12 @@
 package de.exxcellent.challenge;
 
-import java.util.List;
-
 public interface ChallengeTask {
 	
+	/** Executes a (query) task on a data table. Implementions can be made for any given task,<br>
+	 *  and then registered inside an instance of {@link ChallengeTaskExecutor},<br>
+	 *  which can dispatch command line arguments to the right task executor depending on the arguments given.
+	 * 
+	 * @param dataTable the {@link FixedSizeTable} instance to base the task upon
+	 */
 	public void executeTask(FixedSizeTable dataTable);
 }
