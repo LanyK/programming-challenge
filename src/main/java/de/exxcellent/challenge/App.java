e5a53dcf9641afd0975278d1a71f0829022fc8bf
Kaiser <yannick-kaiser@gmx.de>
2021-11-11 22:11:05 +0100

    App.java implements Weather Challenge

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,10 +1,13 @@
 package de.exxcellent.challenge;
 
+import java.io.IOException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Yannick Kaiser <yannick-kaiser@gmx.de>
  */
 public final class App {
 
@@ -14,12 +17,19 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+    	ChallengeTaskExecutor taskExecutor = new ChallengeTaskExecutor();
+    	taskExecutor.registerTask("weather", new WeatherDataChallenge());
+    	
+    	try {
+			taskExecutor.executeTask(args);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+//        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+//        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+//
+//        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+//        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
